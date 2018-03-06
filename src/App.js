cb5f8ebd6a7028a463c5ce534d1d92485fedc9d8
Mike Martin <mikesdigitaldesign@gmail.com>
2018-03-06 14:10:43 -0500

    changing host

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-    fetch("http://localhost:3000/imageurl", {
+    fetch("https://immense-sea-99510.herokuapp.com/imageurl", {
       method: "put",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -98,7 +98,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch("http://localhost:3000/image", {
+          fetch("https://immense-sea-99510.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
