e18a811ca11a351a42cb5cd7de2fa3834da8be09
Mike Martin <mikesdigitaldesign@gmail.com>
2018-03-06 17:31:46 -0500

    Removed some unused files

--- notes.js
+++
@@ -1,7 +0,0 @@
-/**
- * Security: Handle user information in a secure fashion.
- * Bcrypt: Very powerful hashing library for JavaScript
- * POST: Passwords always in POST
- * Bcrypt: Hash passwords and compare them
- * HTTPS: Always have to use HTTPS with passwords.
- */
